293a18e623a29203f6f11f0892c015d48b8dfa42
alexvadill23 <alexvadill23@gmail.com>
2025-06-22 20:42:36 +0200

    Add files via upload

---
+++ pinn.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+# Clase principal para la PINN
+class SchrodingerPINN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(1, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh(),
+            nn.Linear(64, 64),
+            nn.Tanh(),
+            nn.Linear(64, 1)
+        )
+        self.E = nn.Parameter(torch.tensor([0.0], dtype=torch.float32))
+        self.net.apply(self.init_weights)
+
+    def init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            nn.init.zeros_(m.bias)
+
+    def forward(self, x):
+        return self.net(x)
